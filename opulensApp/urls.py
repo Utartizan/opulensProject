88461bb5bae5a06e324c1fa74bd7d6e4e244d293
Utartizan <u2281887@unimail.hud.ac.uk>
2024-10-05 02:37:18 +0100

    changes

--- opulensApp/urls.py
+++ opulensApp/urls.py
@@ -4,5 +4,12 @@ from . import views
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('opulens/', views.opulens, name='opulens'),
-
+    path('reportsinsights/', views.reportsinsights, name='reportsinsights'),
+    path('signin/', views.signin, name='signin'),
+    path('userinput/', views.userinput, name='userinput'),
+    path('userprofile/', views.userprofile, name='userprofile'),
+    path('usersettings/', views.usersettings, name='usersettings'),
+    path('logout/', views.logout, name='logout'),
+    path('datavisualization/', views.datavisualization, name='datavisualization'),
+    path('budgetracking/', views.budgetracking, name='budgetracking'),
 ]
